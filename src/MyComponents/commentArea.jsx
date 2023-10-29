7800c681d2aa8ae994ac090af9cd20e753daff00
WillyWilliam00 <133374335+WillyWilliam00@users.noreply.github.com>
2023-10-29 12:08:50 +0100

    Update commentArea.jsx

--- src/MyComponents/commentArea.jsx
+++ src/MyComponents/commentArea.jsx
@@ -17,7 +17,7 @@ export default  function CommentArea({asin}){
 
         fetch(`https://striveschool-api.herokuapp.com/api/comments/${asin}`, {
         headers: {
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTM3YWIzZmU3NDZhMDAwMTQ4MTQzMmEiLCJpYXQiOjE2OTg0MTQ4MTMsImV4cCI6MTY5OTYyNDQxM30.MEt2ISJjBifylYxkGyIKwaJbmj-MHdsB0Dvrr7rGluc"
+        "Authorization": "codice"
         }})
         .then(r => r.json())
         .then(setAllComment)
